48f01eef33cbe58e2885afc8847c65423b5a174e
lonnen <chris.lonnen@gmail.com>
2021-02-18 17:01:25 -0800

    towards a repl
    we want more than one command, so let's move things into a loop. You'll need to use a keyboard signal to break out of this.
    
    rust flushes stdout when it hits a newline, so println and print have a slightly different behavior and we need to force a fush to create a prod.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
+use std::io::Write;
+use std::io::{stdin, stdout};
 use std::process::Command;
-use std::io::stdin;
 
 fn main() {
     let mut input = String::new();
-    stdin().read_line(&mut input).unwrap();
+    loop {
+        print!("$ ");
+        stdout().flush().ok().expect("Could not flush stdout");
 
-    Command::new(input.trim())
-        .spawn()
-        .unwrap();
+        //stdin().read_line(&mut input).unwrap();
+
+        //Command::new(input.trim())
+        //    .spawn()
+        //    .unwrap();
+    }
 }
