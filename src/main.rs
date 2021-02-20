37e16f99900027f7795ef0f6cfc51f0412a6ee87
lonnen <chris.lonnen@gmail.com>
2021-02-19 23:57:30 -0800

    we want to be able to run a multiple commands in serial

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,18 @@ use std::io::{stdin, stdout};
 use std::process::Command;
 
 fn main() {
-    let mut input = String::new();
     loop {
         print!("$ ");
         stdout().flush().ok().expect("Could not flush stdout");
 
-        //stdin().read_line(&mut input).unwrap();
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
 
-        //Command::new(input.trim())
-        //    .spawn()
-        //    .unwrap();
+        let mut child = Command::new(input.trim())
+           .spawn()
+           .unwrap();
+
+        // wait for the command
+        child.wait().ok().expect("1");
     }
 }
